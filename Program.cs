c7553da361080909dc66bbe7f31eca6f4225edde
Manoel Afonso <mano.afonso93@gmail.com>
2020-01-31 21:16:09 -0300

    Creating project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_worked_hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
